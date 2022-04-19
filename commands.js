e5852d915b8e0a6e971ff9a8edbec32ef8df8b1c
Eddy-Main <eddy.ly.urang@gmail.com>
2022-04-19 18:03:30 +0200

    Added DisguisedBot html presentation.

--- commands.js
+++ commands.js
@@ -581,9 +581,9 @@ class Leaderboard extends Command {
             let point = listNamesPoints[1][i];
 
             if (boardMessage == "")
-                boardMessage = "`" + (name.slice(0, 30).padEnd(40, ".").concat(point)) + "`";
+                boardMessage = "`" + (name.slice(0, 25).padEnd(25, ".").concat((point.toString()).padStart(5, "."))) + "`";
             else
-                boardMessage += "\n`" + (name.slice(0, 30).padEnd(40, ".").concat(point)) + "`";
+                boardMessage += "\n`" + (name.slice(0, 25).padEnd(25, ".").concat((point.toString()).padStart(5, "."))) + "`";
         }
 
 
